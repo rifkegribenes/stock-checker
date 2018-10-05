995eae6fc5a99d673b7002cc09884c9b5f8bab48
rifkegribenes <rifkegribenes@gmail.com>
2018-10-05 21:47:00 +0000

    👀🌲 Checkpoint ./routes/api.js:417995/465 ./tests/2_functional-tests.js:417995/1081

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -18,7 +18,7 @@ chai.use(chaiHttp);
 
 let _id1;
 let _id2;
-let firstLikes;
+let likes;
 
 suite('Functional Tests', function() {
     
@@ -51,7 +51,7 @@ suite('Functional Tests', function() {
               assert.property(res.body, 'price', 'StockData should contain price');
               assert.property(res.body, 'likes', 'StockData should contain likes');
               assert.isAtLeast(res.body.likes, 1, 'likes should be at least 1');
-              firstLikes = res.body.likes;
+              likes = res.body.likes;
               assert.equal(res.body.stock, 'GOOG');
               done();
             });
@@ -69,7 +69,7 @@ suite('Functional Tests', function() {
               assert.property(res.body, 'price', 'StockData should contain price');
               assert.property(res.body, 'likes', 'StockData should contain likes');
               assert.isAtLeast(res.body.likes, 1, 'likes should be at least 1');
-              assert.equal(res.body.likes, firstLikes);
+              assert.equal(res.body.likes, likes);
               assert.equal(res.body.stock, 'GOOG');
               done();
             });
@@ -93,7 +93,21 @@ suite('Functional Tests', function() {
       });
       
       test('2 stocks with like', function(done) {
-        
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({stock: ['aapl', 'goog']})
+          .end((err, res) => {
+              assert.equal(res.status, 200);
+              assert.isArray(res.body, 'Res.body should be an array');
+              assert.property(res.body[0], 'stock', 'Each StockData object should contain stock');
+              assert.property(res.body[0], '_id', 'Each StockData object should contain _id');
+              _id2 = res.body[0]._id;
+              assert.property(res.body[0], 'price', 'Each StockData object should contain price');
+              assert.property(res.body[0], 'rel_likes', 'Each StockData object should contain rel_likes');
+              assert.isAtLeast(res.body.likes, 1, 'likes should be at least 1');
+              assert.equal(res.body[0].stock, 'AAPL');
+              done();
+            }); 
       });
       
     });
