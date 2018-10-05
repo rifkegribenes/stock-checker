4ce8e59a69a72b1eb7b7643d9dec80b7082ad3aa
rifkegribenes <rifkegribenes@gmail.com>
2018-10-05 06:08:53 +0000

    ⛵️🚌 Checkpoint ./routes/api.js:417995/1452

--- routes/api.js
+++ routes/api.js
@@ -16,9 +16,6 @@ const handleError = (res, err) => {
   return res.status(500).json({message: err});
 }
 
-// here's where we'll save and compare the number of likes if two stocks are submitted
-let likesCompare = [];
-
 // here's where result data goes if two stocks were submitted
 let result = [];
 
@@ -45,23 +42,34 @@ const getStockData = (stocks) => {
 const saveStock = (stockToSave, single) => {
   stockToSave.save()
     .then((savedStock) => {
-      // create the result object and return to client
+      // create the stockData object to return to client
       const stockData = { ...savedStock._doc };
       stockData.likes = savedStock._doc.likeIPs.length;
       // if this was the only stock submitted, return it now
       if (single) {
         return res.status(200).json({ stockData });
       } else {
-        // otherwise, save it to the result object
+        // otherwise, save it to the result array
         result.push(stockData);
         // if the result array already has two stocks in it, it's time to return it
         if (result.length === 2) {
-          // first map through the two objects and substitute the likesCompare value for the # of likes
-          const resultToReturn = result.map((stockObj) => {
-            // for each stock object
-            // find the relative likes value
-            stockObj
-          }
+          // first map through the two objects and substitute the rel_likes value for the # of likes
+          const resultToReturn = result.map((stockObj, index) => {
+            // save a reference to the 'other stock' in the array
+            // (not the one being processed at this step in the map)
+            const otherStock = index === 0 ? result[1] : result[0];
+            // find the relative likes value by comparing this object to the otherStock
+            stockObj.rel_likes = stockObj.likes - otherStock.likes;
+            // then delete the likes key
+            delete stockObj.likes;
+            // return the updated stockObject
+            return { stockData: stockObj }
+          });
+          // return this array of stockObjects to the client
+          return res.status(200).json(resultToReturn);
+        } else {
+          // otherwise, just return out of this function and continue
+          return;
         }
       }
     })
@@ -99,11 +107,8 @@ module.exports = function (app) {
               // if the stock is not yet in the db
                 if (!stockFromMongo) {
                   // if it was liked, save the liker's IP in the likeIPs array
-                  // and add a value of 1 (this is the first like for this stock) 
-                  // to the likesCompare array
                   if (like) {
                     likeIPs.push(likeIP);
-                    likesCompare.push(1);
                   }
                   // create the new stock object to save to mongo
                   let newStock = new Stock({
@@ -112,7 +117,7 @@ module.exports = function (app) {
                     likeIPs
                   });
                   // and save it
-                  
+                  saveStock(newStock, single);
                   } else {
                   // if the stock already exists in mongo
                   // make a copy to avoid mutation
