5523600a61c9ed2ea2e4daa0f716b9837f467146
rifkegribenes <rifkegribenes@gmail.com>
2018-10-05 05:13:52 +0000

    👨‍❤️‍💋‍👨⚓️ Checkpoint ./routes/api.js:417995/3095

--- routes/api.js
+++ routes/api.js
@@ -19,10 +19,6 @@ const handleError = (res, err) => {
 const getStockData = (stocks) => {
 
   return new Promise((resolve, reject) => {
-    const now = new Date();
-    const YYYY = now.getFullYear();
-    const MM = now.getMonth() + 1;
-    const DD = now.getDate();
     
     const url = `https://api.iextrading.com/1.0/stock/market/batch?symbols=${stocks}&types=quote&last=1`;
     
@@ -44,22 +40,22 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get((req, res) => {
-      const { stock } = req.query;
+      const { stock, like } = req.query;
 
       getStockData(stock)
         .then((data) => {
           const stockList = Object.keys(data);
           console.log(stockList);
-          let stockDataArray = [];
-          stockList.forEach((stockObj) => {
-            stockDataArray
-          }
-         data.quote.iexRealtimePrice
-          const newStock = new Stock({
-            stock,
-            price,
-            likesIPs: []
-          });
+          let stockDataReturnArray = [];
+          stockList.forEach((stockKey) => {
+            Stock.findOne({ stock: stockKey })
+              .then((stockFromMongo) => {
+                if (!stockFromMongo) {
+                  let newStock = new Stock({
+                    stock,
+                    price: data[stockKey].quote.iexRealtimePrice,
+                    likesIPs: []
+                  });
           newStock.save()
             .then((stockData) => {
               const stock = { ...stockData._doc };
@@ -69,7 +65,13 @@ module.exports = function (app) {
             .catch((err) => {
               console.log(`api.js > get newStock.save ${err}`);
               return handleError(res, err);
-            });         
+            });
+                }
+            })
+              .catch();
+            
+          }
+                 
         })
         .catch((err) => {
           console.log(`api.js > get utils.getContent ${err}`);
